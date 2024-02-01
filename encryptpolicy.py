5da8170e9f20e252d58b4bc12e77ef802a11d4d6
I-am-Oak <satyaprakash.me612@gmail.com>
2024-02-01 15:43:22 +0530

    Initial Commit

---
+++ encryptpolicy.py
@@ -0,0 +1,26 @@
+from cryptography.fernet import Fernet
+# key generation
+key = Fernet.generate_key()
+
+# storing the key in a file
+with open('filekey.key', 'wb') as filekey:
+    filekey.write(key)
+
+# opening the key
+with open('filekey.key', 'rb') as filekey:
+	key = filekey.read()
+
+# using the generated key
+fernet = Fernet(key)
+
+# opening the original file to encrypt
+with open('policy.csv', 'rb') as file:
+	original = file.read()
+	
+# encrypting the file
+encrypted = fernet.encrypt(original)
+
+# opening the file in write mode and 
+# writing the encrypted data
+with open('policy.csv', 'wb') as encrypted_file:
+	encrypted_file.write(encrypted)
